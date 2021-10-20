c80acd5e01ac6ba6e3bd47321641baccebeb43a9
Eduardo Garcia Massam <eduardogarciamassam@hotmail.com>
2021-10-20 20:25:04 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace celsius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+           
+
+            Console.WriteLine("informe numero:");
+            numero = int.Parse(Console.ReadLine());
+
+            //  Console.WriteLine("sucessor" + (numero+1));
+            //  Console.WriteLine("antecessor :" + (numero-1));
+            Console.WriteLine("O número digitado foi {0}, o Antecessor {1} e o Sucessor {2} e o Teste {3}", numero, numero -= 1, numero += 2, numero-=2);
+        }
+    }
+}
